993d2a62a6966244467180484e18678946b87bb7
Mario <>
2023-11-07 15:13:51 -0700

    se agregaron las variables en la clase customer.

--- src/mx/itson/catrina/entities/Customer.java
+++ src/mx/itson/catrina/entities/Customer.java
@@ -10,4 +10,64 @@ package mx.itson.catrina.entities;
  */
 public class Customer {
     
+    private int id;
+    private String name;
+    private String adress;
+    private String city;
+    private String rfc;
+    private int zip_code;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public int getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(int zip_code) {
+        this.zip_code = zip_code;
+    }
+    
+    
+    
+    
 }
+
+
