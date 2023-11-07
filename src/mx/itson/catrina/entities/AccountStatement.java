d09af48ee0520ec27b27a4270f2f06dc500de303
Luis Blasco @LuxhPc <luism@LuxhPC>
2023-11-07 15:11:07 -0700

    Se agregaron las variables correspondientes en la clase AccountStatement.java

--- src/mx/itson/catrina/entities/AccountStatement.java
+++ src/mx/itson/catrina/entities/AccountStatement.java
@@ -4,10 +4,58 @@
  */
 package mx.itson.catrina.entities;
 
+import java.util.List;
+
 /**
  *
  * @author luism
  */
 public class AccountStatement {
+    private int account;
+    private int clabe;
+    private String currency;
+    private List<Customer> customer;
+    private List<Transactions> transactions;
+
+    public int getAccount() {
+        return account;
+    }
+
+    public void setAccount(int account) {
+        this.account = account;
+    }
+
+    public int getClabe() {
+        return clabe;
+    }
+
+    public void setClabe(int clabe) {
+        this.clabe = clabe;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public List<Customer> getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(List<Customer> customer) {
+        this.customer = customer;
+    }
+
+    public List<Transactions> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transactions> transactions) {
+        this.transactions = transactions;
+    }
+    
     
 }
