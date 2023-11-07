c6c3e6a0c6cb44a57259cdb2a94773c003d110ab
Luis Blasco @LuxhPc <luism@LuxhPC>
2023-11-07 14:56:31 -0700

    Se agregaron las variables correspondientes en la clase Transactions.java

--- src/mx/itson/catrina/entities/Transactions.java
+++ src/mx/itson/catrina/entities/Transactions.java
@@ -9,5 +9,42 @@ package mx.itson.catrina.entities;
  * @author luism
  */
 public class Transactions {
+    private String date;
+    private String description;
+    private int amount;
+    private int type;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+    
     
 }
